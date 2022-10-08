fa9f998547ce08e08ee5894e3764748bdf59b1aa
“Vince <vmemmo1@gmail.com>
2022-10-08 15:27:21 -0700

    pushing kick.mps to github

--- src/scripts/kick.js
+++ src/scripts/kick.js
@@ -1,14 +1,20 @@
 import * as Tone from 'tone'
 console.log('kick working!')
-
+import { Player } from 'tone';
 
 class Kick {
     constructor () {
         this.setUpKickArray = this.setUpKickArray.bind(this)
         this.setupDrumssSelection = this.setupDrumsSelection.bind(this)
     }
-
+    
     setUpKickArray() {
         
     }
+
+    setKick() {
+        const kick = new Player("https://kernapillar.github.io/4-block-loop/src/drum_samples/hihat2.mp3").toDestination();
+    }
+    
+
 }
